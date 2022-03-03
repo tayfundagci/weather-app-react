bab22608e498ea1b2fd505b7fa640822d53a46ec
tayfundagci <tayfundagci3517@gmail.com>
2022-03-03 10:15:00 +0300

    test

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,14 +3,19 @@ import React from "react";
 function Footer() {
   return (
     <div>
-      <p style={{ textAlign: "center", marginTop: "10px" }}>
+      <p
+        style={{
+          textAlign: "center",
+          marginTop: "10px",
+        }}
+      >
         Developed by{" "}
         <a
           href="https://www.linkedin.com/in/tayfundagci/"
           style={{ fontWeight: "bold" }}
           target="_blank"
         >
-          Tayfun Dağcı
+          Tayfun Dagci
         </a>
         <br />
       </p>
