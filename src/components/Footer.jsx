e1b9b56a01892dd3b4e7ed91e535ad43a24f254d
tayfundagci <tayfundagci3517@gmail.com>
2022-03-03 18:16:38 +0300

    footer changed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,5 @@
+import { BsGithub } from "react-icons/bs";
+import { AiFillLinkedin } from "react-icons/ai";
 import React from "react";
 
 function Footer() {
@@ -19,6 +21,21 @@ function Footer() {
         </a>
         <br />
       </p>
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          marginTop: "5px",
+        }}
+      >
+        <a href="https://www.github.com/tayfundagci/" target="blank">
+          <BsGithub size={"33"} />
+        </a>
+        <a href="https://www.linkedin.com/in/tayfundagci/" target="blank">
+          <AiFillLinkedin size={"40"} h />
+        </a>
+      </div>
     </div>
   );
 }
